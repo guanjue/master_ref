d1160e8df208913d2f69046a1a8849975bc2a54a
Universe <universe@MacBook-Air.local>
2017-02-06 02:34:39 -0500

    procap script

---
+++ bin/nfr_based_pip_procap.sh
@@ -0,0 +1,91 @@
+
+cd /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/procap_refs/
+
+#cat Xu_2009_ORF-Ts_V64_sort.noexpan.bed | awk -F '\t' -v OFS='\t' '{if ($6=="+") print $1,$2-500,$2+501,$4,$5,$6; else print $1,$3-500,$3+501,$4,$5,$6}' > Xu_2009_ORF-Ts_V64_sort_TSS1k.bed
+#java -jar /Users/universe/Documents/bin/cegr-tools/build/dist/TagPileup.jar -b /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/procap_proseq_bam/SRR3031844_1.qt.adpt.36.fastq.mapped.sort.bam -i /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/procap_proseq_bam/SRR3031844_1.qt.adpt.36.fastq.mapped.sort.bam.bai -c Xu_2009_ORF-Ts_V64_sort_TSS1k.bed -s 0 -n 1 -e false -r 0 -p false -a 1 -t 3 -w 0 -h true -m false
+#java -jar /Users/universe/Documents/bin/cegr-tools/build/dist/TagPileup.jar -b /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/procap_proseq_bam/SRR3031845_1.qt.adpt.36.fastq.mapped.sort.bam -i /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/procap_proseq_bam/SRR3031845_1.qt.adpt.36.fastq.mapped.sort.bam.bai -c Xu_2009_ORF-Ts_V64_sort_TSS1k.bed -s 0 -n 1 -e false -r 0 -p false -a 1 -t 3 -w 0 -h true -m false
+
+#python /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/master_ref/bin/subtract_bg.py -s Xu_2009_ORF-Ts_V64_sort_TSS1k_SRR3031844_1_read1_combined.tabular -b Xu_2009_ORF-Ts_V64_sort_TSS1k_SRR3031845_1_read1_combined.tabular -o Xu_2009_ORF-Ts_V64_sort_TSS1k_SRR3031844_SRR3031845_1_read1_combined.tabular
+
+#python /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/master_ref/bin/relocate_TSS.py -b Xu_2009_ORF-Ts_V64_sort_TSS1k.bed -r Xu_2009_ORF-Ts_V64_sort_TSS1k_SRR3031844_SRR3031845_1_read1_combined.tabular -o Xu_2009_ORF-Ts_V64_sort_TSS2001_procap.bed -t 50 -w 1000
+
+#java -jar /Users/universe/Documents/bin/cegr-tools/build/dist/TagPileup.jar -b /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/procap_proseq_bam/SRR3031844_1.qt.adpt.36.fastq.mapped.sort.bam -i /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/procap_proseq_bam/SRR3031844_1.qt.adpt.36.fastq.mapped.sort.bam.bai -c Xu_2009_ORF-Ts_V64_sort_TSS2001_procap.bed.2000.bed -s 0 -n 1 -e false -r 0 -p false -a 0 -t 3 -w 0 -h true -m false
+
+
+
+
+#python /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/master_ref/bin/get_NFR.py -f Xu_2009_ORF-Ts_V64_sort_TSS2001_procap_h3_merged_MN_seq_sort_read1_combined.cdt -w 100 -s 25 -l 79 -u 1000 -d 0
+
+paste Xu_2009_ORF-Ts_V64_sort_TSS2001_procap.bed.2000.bed Xu_2009_ORF-Ts_V64_sort_TSS2001_procap_h3_merged_MN_seq_sort_read1_combined.cdt.nucleosome.txt > Xu_2009_ORF-Ts_V64_sort_h3_merged_MN_seq_sort_read1_combined.cdt.nucleosome.txttmp
+
+sort -k10,10n Xu_2009_ORF-Ts_V64_sort_h3_merged_MN_seq_sort_read1_combined.cdt.nucleosome.txttmp > Xu_2009_ORF-Ts_V64_sort_h3_merged_MN_seq_sort_read1_combined.cdt.nucleosome.txttmpsort
+
+cat Xu_2009_ORF-Ts_V64_sort_h3_merged_MN_seq_sort_read1_combined.cdt.nucleosome.txttmpsort | awk -F '\t' -v OFS='\t' '{if ($6=="+" && $7!="na") print $1,$2+$7-500,$2+$7+501,$4,$5,$6; else if ($6=="-" && $7!="na") print $1,$3-$7-500,$3-$7+501,$4,$5,$6; else if ($7=="na") print $1,($2+$3-1)/2-500,($2+$3-1)/2+501,$4,$5,$6}' > Xu_2009_ORF-Ts_V64_sort_nfr_minus1_nfrbased_sort.bed
+
+cat Xu_2009_ORF-Ts_V64_sort_h3_merged_MN_seq_sort_read1_combined.cdt.nucleosome.txttmpsort | awk -F '\t' -v OFS='\t' '{if ($6=="+" && $8!="na") print $1,$2+$8-500,$2+$8+501,$4,$5,$6; else if ($6=="-" && $8!="na") print $1,$3-$8-500,$3-$8+501,$4,$5,$6; else if ($8=="na") print $1,($2+$3-1)/2-500,($2+$3-1)/2+501,$4,$5,$6}' > Xu_2009_ORF-Ts_V64_sort_h3_merged_MN_seq_sort_read1_combined_nfr.bed
+
+cat Xu_2009_ORF-Ts_V64_sort_h3_merged_MN_seq_sort_read1_combined.cdt.nucleosome.txttmpsort | awk -F '\t' -v OFS='\t' '{if ($6=="+" && $9!="na") print $1,$2+$9-500,$2+$9+501,$4,$5,$6; else if ($6=="-" && $9!="na") print $1,$3-$9-500,$3-$9+501,$4,$5,$6; else if ($9=="na") print $1,($2+$3-1)/2-500,($2+$3-1)/2+501,$4,$5,$6}' > Xu_2009_ORF-Ts_V64_sort_nfr_plus1_nfrbased_sort.bed
+
+cat Xu_2009_ORF-Ts_V64_sort_nfr_plus1_nfrbased_sort.bed | awk '{print $4}' > nfr_based_genename_order_nfrbased.txt
+
+
+
+rm -r compare_plus1_nfrbased
+mkdir compare_plus1_nfrbased 
+mv Xu_2009_ORF-Ts_V64_sort_nfr_minus1_nfrbased_sort.bed compare_plus1_nfrbased
+mv Xu_2009_ORF-Ts_V64_sort_nfr_plus1_nfrbased_sort.bed compare_plus1_nfrbased
+mv nfr_based_genename_order_nfrbased.txt compare_plus1_nfrbased
+cp gene_group_list.txt compare_plus1_nfrbased
+cp ScriptManager-v0.10.jar compare_plus1_nfrbased
+
+cd compare_plus1_nfrbased
+mkdir nfr_based_nfrsort
+java -jar /Users/universe/Documents/bin/cegr-tools/build/dist/TagPileup.jar -b /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/pip_seq_bam/56422_T.uniq.bam -i /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/pip_seq_bam/56422_T.uniq.bam.bai -c Xu_2009_ORF-Ts_V64_sort_nfr_minus1_nfrbased_sort.bed -s 0 -n 1 -e false -r 0 -p false -a 1 -t 3 -w 0 -h true -m false 
+java -jar /Users/universe/Documents/bin/cegr-tools/build/dist/TagPileup.jar -b /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/pip_seq_bam/56422_T.uniq.bam -i /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/pip_seq_bam/56422_T.uniq.bam.bai -c Xu_2009_ORF-Ts_V64_sort_nfr_plus1_nfrbased_sort.bed -s 0 -n 1 -e false -r 0 -p false -a 1 -t 3 -w 0 -h true -m false 
+mv Xu_2009_ORF-Ts_V64_sort_nfr_minus1_nfrbased_sort_56422_T_read1_combined.tabular nfr_based_nfrsort
+#mv Xu_2009_ORF-Ts_V64_sort_nfr_minus1_nfrbased_sort_56422_T_pip_seq.txt nfr_based_nfrsort
+mv Xu_2009_ORF-Ts_V64_sort_nfr_plus1_nfrbased_sort_56422_T_read1_combined.tabular nfr_based_nfrsort
+#mv Xu_2009_ORF-Ts_V64_sort_nfr_plus1_nfrbased_sort_56422_T_pip_seq.txt nfr_based_nfrsort
+
+java -jar /Users/universe/Documents/bin/cegr-tools/build/dist/TagPileup.jar -b /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/procap_proseq_bam/SRR3031844_1.qt.adpt.36.fastq.mapped.sort.bam -i /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/procap_proseq_bam/SRR3031844_1.qt.adpt.36.fastq.mapped.sort.bam.bai -c Xu_2009_ORF-Ts_V64_sort_nfr_minus1_nfrbased_sort.bed -s 0 -n 1 -e false -r 0 -p false -a 0 -t 3 -w 0 -h true -m false 
+java -jar /Users/universe/Documents/bin/cegr-tools/build/dist/TagPileup.jar -b /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/procap_proseq_bam/SRR3031844_1.qt.adpt.36.fastq.mapped.sort.bam -i /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/procap_proseq_bam/SRR3031844_1.qt.adpt.36.fastq.mapped.sort.bam.bai -c Xu_2009_ORF-Ts_V64_sort_nfr_plus1_nfrbased_sort.bed -s 0 -n 1 -e false -r 0 -p false -a 0 -t 3 -w 0 -h true -m false 
+mv Xu_2009_ORF-Ts_V64_sort_nfr_minus1_nfrbased_sort_SRR3031844_1_read1_*.tabular nfr_based_nfrsort
+mv Xu_2009_ORF-Ts_V64_sort_nfr_plus1_nfrbased_sort_SRR3031844_1_read1_*.tabular nfr_based_nfrsort
+
+cd nfr_based_nfrsort
+#Rscript /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/master_ref/bin/heatmap.R Xu_2009_ORF-Ts_V64_sort_nfr_minus1_nfrbased_sort_56422_T_read1_combined.tabular Xu_2009_ORF-Ts_V64_sort_nfr_minus1_nfrbased_sort_56422_T_read1_combined.png
+#Rscript /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/master_ref/bin/heatmap.R Xu_2009_ORF-Ts_V64_sort_nfr_plus1_nfrbased_sort_56422_T_read1_combined.tabular Xu_2009_ORF-Ts_V64_sort_nfr_plus1_nfrbased_sort_56422_T_read1_combined.png
+cd ..
+
+mkdir nfr_based_nfrsort_genegroup
+
+python /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/master_ref/bin/gene_group_split.py -t Xu_2009_ORF-Ts_V64_sort_nfr_minus1_nfrbased_sort.bed -a 3 -g gene_group_list.txt -b 0 -i F
+python /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/master_ref/bin/gene_group_split.py -t Xu_2009_ORF-Ts_V64_sort_nfr_plus1_nfrbased_sort.bed -a 3 -g gene_group_list.txt -b 0 -i F
+
+mv Xu_2009_ORF-Ts_V64_sort_nfr_minus1_nfrbased_sort.bed.genegroup Xu_2009_ORF-Ts_V64_sort_nfr_minus1_nfrbased_sort_genegroup.bed
+mv Xu_2009_ORF-Ts_V64_sort_nfr_plus1_nfrbased_sort.bed.genegroup Xu_2009_ORF-Ts_V64_sort_nfr_plus1_nfrbased_sort_genegroup.bed
+
+java -jar /Users/universe/Documents/bin/cegr-tools/build/dist/TagPileup.jar -b /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/pip_seq_bam/56422_T.uniq.bam -i /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/pip_seq_bam/56422_T.uniq.bam.bai -c Xu_2009_ORF-Ts_V64_sort_nfr_minus1_nfrbased_sort_genegroup.bed -s 0 -n 1 -e false -r 0 -p false -a 1 -t 3 -w 0 -h true -m false 
+java -jar /Users/universe/Documents/bin/cegr-tools/build/dist/TagPileup.jar -b /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/pip_seq_bam/56422_T.uniq.bam -i /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/pip_seq_bam/56422_T.uniq.bam.bai -c Xu_2009_ORF-Ts_V64_sort_nfr_plus1_nfrbased_sort_genegroup.bed -s 0 -n 1 -e false -r 0 -p false -a 1 -t 3 -w 0 -h true -m false 
+mv Xu_2009_ORF-Ts_V64_sort_nfr_minus1_nfrbased_sort_genegroup_56422_T_read1_combined.tabular nfr_based_nfrsort_genegroup
+#mv Xu_2009_ORF-Ts_V64_sort_nfr_minus1_nfrbased_sort_genegroup_56422_T_pip_seq.txt nfr_based_nfrsort_genegroup
+mv Xu_2009_ORF-Ts_V64_sort_nfr_plus1_nfrbased_sort_genegroup_56422_T_read1_combined.tabular nfr_based_nfrsort_genegroup
+#mv Xu_2009_ORF-Ts_V64_sort_nfr_plus1_nfrbased_sort_genegroup_56422_T_pip_seq.txt nfr_based_nfrsort_genegroup
+
+java -jar /Users/universe/Documents/bin/cegr-tools/build/dist/TagPileup.jar -b /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/procap_proseq_bam/SRR3031844_1.qt.adpt.36.fastq.mapped.sort.bam -i /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/procap_proseq_bam/SRR3031844_1.qt.adpt.36.fastq.mapped.sort.bam.bai -c Xu_2009_ORF-Ts_V64_sort_nfr_minus1_nfrbased_sort_genegroup.bed -s 0 -n 1 -e false -r 0 -p false -a 0 -t 3 -w 0 -h true -m false 
+java -jar /Users/universe/Documents/bin/cegr-tools/build/dist/TagPileup.jar -b /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/procap_proseq_bam/SRR3031844_1.qt.adpt.36.fastq.mapped.sort.bam -i /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/procap_proseq_bam/SRR3031844_1.qt.adpt.36.fastq.mapped.sort.bam.bai -c Xu_2009_ORF-Ts_V64_sort_nfr_plus1_nfrbased_sort_genegroup.bed -s 0 -n 1 -e false -r 0 -p false -a 0 -t 3 -w 0 -h true -m false 
+mv Xu_2009_ORF-Ts_V64_sort_nfr_minus1_nfrbased_sort_genegroup_SRR3031844_1_read1_*.tabular nfr_based_nfrsort_genegroup
+mv Xu_2009_ORF-Ts_V64_sort_nfr_plus1_nfrbased_sort_genegroup_SRR3031844_1_read1_*.tabular nfr_based_nfrsort_genegroup
+
+cd nfr_based_nfrsort_genegroup
+#Rscript /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/master_ref/bin/heatmap.R Xu_2009_ORF-Ts_V64_sort_nfr_minus1_nfrbased_sort_genegroup_56422_T_read1_combined.tabular Xu_2009_ORF-Ts_V64_sort_nfr_minus1_nfrbased_sort_genegroup_56422_T_read1_combined.png
+#Rscript /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/master_ref/bin/heatmap.R Xu_2009_ORF-Ts_V64_sort_nfr_plus1_nfrbased_sort_genegroup_56422_T_read1_combined.tabular Xu_2009_ORF-Ts_V64_sort_nfr_plus1_nfrbased_sort_genegroup_56422_T_read1_combined.png
+cd ..
+
+
+mkdir old_plus1
+cd old_plus1
+echo 'Yeast_plus_one_sacCer3_TSS_distsort.bed' > gene_order.txt
+python /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/master_ref/bin/gene_match.py -t Xu_2009_ORF-Ts_V64_sort_nfr_plus1_nfrbased_sort_genegroup_h3_merged_MN_seq_sort_read1_combined.cdt -a 0 -g gene_order.txt -b 3 -i T
+python /Volumes/MAC_Data/data/labs/pugh_lab/master_ref/master_ref/bin/gene_sort.py -t Yeast_plus_one_sacCer3_TSS_distsort_h3_merged_MN_seq_sort_read1_combined.cdt -a 0 -r Xu_2009_ORF-Ts_V64_sort_nfr_plus1_nfrbased_sort_genegroup_h3_merged_MN_seq_sort_read1_combined.cdt -b 0 -o Yeast_plus_one_sacCer3_TSS_distsort_h3_merged_MN_seq_sort_read1_combined_nfrbased_sort.cdt
+cd ..
--- bin/subtract_bg.py
+++ bin/subtract_bg.py
@@ -1,49 +1,66 @@
 import numpy as np
 
-data_out=open('ensGeneTSS1kwin_SRR3031844_1_readc_bgsub.tabular','w')
+def subtract_bg(signal_file,bg_file,output_file):
+	data_out=open(output_file,'w')
 
-data0=open('SRR3031844_1/ensGeneTSS1kwin_SRR3031844_1_readc_sense.tabular','r')
-data_out.write(data0.readline())
-data00_name=[]
-data00_reads=[]
-for records in data0:
-	tmp=records.split()
-	data00_name.append(tmp[0])
-	data00_reads.append(tmp[1:])
-data00_reads=np.array(data00_reads)
+	print('read signal data')
+	data0=open(signal_file,'r')
+	data_out.write(data0.readline())
+	data00_name=[]
+	data00_reads=[]
+	for records in data0:
+		tmp=records.split()
+		data00_name.append(tmp[0])
+		data00_reads.append(tmp[2:])
+	data00_reads=np.array(data00_reads,dtype=float)
 
-data1=open('SRR3031845_1/ensGeneTSS1kwin_SRR3031845_1_readc_sense.tabular','r')
-data1.readline()
-data10_reads=[]
-for records in data1:
-	tmp=records.split()
-	data10_reads.append(tmp[1:])
-data10_reads=np.array(data10_reads)
+	print('read background data')
+	data1=open(bg_file,'r')
+	data1.readline()
+	data10_reads=[]
+	for records in data1:
+		tmp=records.split()
+		data10_reads.append(tmp[2:])
+	data10_reads=np.array(data10_reads,dtype=float)
 
-dif_reads=data00_reads-data10_reads
-dif_reads=dif_reads.clip(min=0)
+	print('subtract the background file')
+	dif_reads=data00_reads-data10_reads
+	dif_reads = dif_reads.clip(min=0)
+	print('write bg_subtracted data')
+	for name,reads in zip(data00_name,dif_reads):
+		data_out.write(name+'\t'+name+'\t')
+		for i in range(0,len(reads)-1):
+			data_out.write(str(reads[i])+'\t')
+		data_out.write(str(reads[-1])+'\n')
 
-for name,reads in zip(data00_name,dif_reads):
-	data_out.write(name+'\t'+name+'\t')
-	for read in reads:
-		data_out.write(read+'\t')
-	data_out.write('\n')
+	data_out.close()
+	data0.close()
+	data1.close()
 
-data_out.close()
-data0.close()
-data1.close()
+############################################################################
+import getopt
+import sys
+def main(argv):
+	try:
+		opts, args = getopt.getopt(argv,"hs:b:o:")
+	except getopt.GetoptError:
+		print 'python subtract_bg.py -s signal_file -b background_file -o output_file'
+		sys.exit(2)
+
+	for opt,arg in opts:
+		if opt=="-h":
+			print 'python subtract_bg.py -s signal_file -b background_file -o output_file'
+			sys.exit()
+		elif opt=="-s":
+			signal_file=str(arg.strip())
+		elif opt=="-b":
+			bg_file=str(arg.strip())
+		elif opt=="-o":
+			output_file=str(arg.strip())
+
+	subtract_bg(signal_file,bg_file,output_file)
+
+if __name__=="__main__":
+	main(sys.argv[1:])
 
 
-
-
-# reads processed: 10779040
-# reads with at least one reported alignment: 1900389 (17.63%)
-# reads that failed to align: 305903 (2.84%)
-# reads with alignments suppressed due to -m: 8572748 (79.53%)
-Reported 1900389 alignments to 1 output stream(s)
-
-# reads processed: 10779040
-# reads with at least one reported alignment: 1894434 (17.58%)
-# reads that failed to align: 294435 (2.73%)
-# reads with alignments suppressed due to -m: 8590171 (79.69%)
-Reported 1894434 alignments to 1 output stream(s)
